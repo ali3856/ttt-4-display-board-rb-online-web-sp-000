3347e89746810905d283261c8d3686f75a482192
witty-banner-1950 <asimsali95@gmail.com>
2019-07-12 15:13:30 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,13 +2,12 @@
 # out the current state.
 board_1 = [" "," "," "," "," "," "," "," "," "]
 
-
 def display_board (board)
-  puts "   |   |   "
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
-  puts "   |   |   "
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
   puts "-----------"
-  puts "   |   |   "
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
 display_board (board_1[4] = "X")

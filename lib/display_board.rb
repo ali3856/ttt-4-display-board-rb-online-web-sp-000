2ff21b140c15441fa5fdd21f50c2d95039e2b672
witty-banner-1950 <asimsali95@gmail.com>
2019-07-12 15:14:46 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,6 +1,6 @@
 # Define display_board that accepts a board and prints
 # out the current state.
-board_1 = [" "," "," "," "," "," "," "," "," "]
+
 
 def display_board (board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
@@ -9,5 +9,3 @@ def display_board (board)
   puts "-----------"
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
-
-display_board (board_1[4] = "X")
